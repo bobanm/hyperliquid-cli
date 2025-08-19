dc86caf4c709848c23fc125a969493557ae8eea9
Boban Milošević <milosevic.boban@gmail.com>
2025-08-19 15:58:45 +0200

    Use TablePrinter for rendering tables

--- src/script/get-account.ts
+++ src/script/get-account.ts
@@ -1,4 +1,5 @@
 import * as hl from '@nktkas/hyperliquid'
+import { TablePrinter } from '@bobanm/table-printer'
 import credentials from '../../input/credentials.toml'
 import { removeDecimals } from '../util/helpers'
 
@@ -17,27 +18,30 @@ let totalUnrealizedPnl = 0
 
 if (account.assetPositions.length > 0) {
     console.log('\nPositions\n')
-    console.log('COIN'.padEnd(10), 'AMOUNT'.padStart(8), 'VALUE'.padStart(8).padEnd(11), 'B.PRICE'.padEnd(10), 'LIQUID'.padEnd(9), 'FUNDING'.padStart(7), 'PROFIT'.padStart(9))
-    console.log('-'.repeat(70))
+
+    const positionsTable = new TablePrinter([
+        ['COIN', 'AMOUNT', 'VALUE', 'B.PRICE', 'LIQUID', 'FUNDING', 'PROFIT'],
+    ])
 
     for (const position of account.assetPositions) {
         const p = position.position
         totalUnrealizedPnl += Number(p.unrealizedPnl)
 
-        console.log(
-            p.coin.padEnd(10),
-            p.szi.padStart(8),
-            removeDecimals(p.positionValue).padStart(8).padEnd(11),
-            p.entryPx.padEnd(10),
-            Number(p.liquidationPx ?? 0).toPrecision(5).padEnd(9),
+        positionsTable.addRow([
+            p.coin,
+            p.szi,
+            removeDecimals(p.positionValue),
+            p.entryPx,
+            Number(p.liquidationPx ?? 0).toPrecision(5),
             // Invert funding sign, because the response shows received funding as negative
-            removeDecimals(-Number(p.cumFunding.sinceOpen)).padStart(7),
-            removeDecimals(p.unrealizedPnl).padStart(9),
-        )
+            removeDecimals(-Number(p.cumFunding.sinceOpen)),
+            removeDecimals(p.unrealizedPnl),
+        ])
     }
+
+    console.log(positionsTable.toString())
 }
 
-console.log('\n')
 console.log(`All positions value  $ ${removeDecimals(account.marginSummary.totalNtlPos).padStart(6)}`)
 console.log(`Total unrealized PnL $ ${removeDecimals(totalUnrealizedPnl).padStart(6)}`)
 console.log(`Net account size     $ ${removeDecimals(account.marginSummary.accountValue).padStart(6)}`)
