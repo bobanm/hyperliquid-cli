b888dbc4a8c50e7508855cfff974e162e0dfa676
Boban Milošević <milosevic.boban@gmail.com>
2025-08-19 17:56:45 +0200

    `get-orders` script now uses `TablePrinter` and `nice-utils`

--- src/script/get-orders.ts
+++ src/script/get-orders.ts
@@ -1,7 +1,38 @@
 import * as hl from '@nktkas/hyperliquid'
+import { TablePrinter } from '@bobanm/table-printer'
+import { NiceDate } from '@bobanm/nice-utils'
 import credentials from '../../input/credentials.toml'
 
 const infoClient = new hl.InfoClient({ transport: new hl.HttpTransport() })
 
 const openOrders = await infoClient.openOrders({ user: credentials.address })
-console.log(openOrders)
+
+if (openOrders.length === 0) {
+    console.log('There are no open orders.')
+
+    process.exit(1)
+}
+
+console.log('\nOpen orders\n')
+
+const ordersTable = new TablePrinter([
+    ['DATE', 'COIN', 'SIDE', 'PRICE', 'O.SIZE', 'SIZE'],
+])
+
+let orderCount = 0
+
+for (const order of openOrders) {
+    ordersTable.addRow([
+        new NiceDate(order.timestamp).toFineString(true, true),
+        order.coin,
+        order.side,
+        order.limitPx,
+        order.origSz,
+        order.sz,
+    ])
+
+    orderCount += 1
+}
+
+console.log(ordersTable.toString())
+console.log(`Total ${orderCount} open orders.`)
