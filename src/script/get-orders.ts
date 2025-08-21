884756dad5c519b56358c352cfee12121a12b4a4
Boban Milošević <milosevic.boban@gmail.com>
2025-08-21 02:14:09 +0200

    `get-orders` script now also shows order USD value

--- src/script/get-orders.ts
+++ src/script/get-orders.ts
@@ -1,6 +1,6 @@
 import * as hl from '@nktkas/hyperliquid'
 import { TablePrinter } from '@bobanm/table-printer'
-import { NiceDate } from '@bobanm/nice-utils'
+import { NiceDate, NiceNumber } from '@bobanm/nice-utils'
 import { infoClient } from '../clients'
 import credentials from '../../input/credentials.toml'
 
@@ -15,7 +15,7 @@ if (openOrders.length === 0) {
 console.log('\nOpen orders\n')
 
 const ordersTable = new TablePrinter([
-    ['DATE', 'COIN', 'SIDE', 'PRICE', 'O.SIZE', 'SIZE', 'ID'],
+    ['DATE', 'COIN', 'SIDE', 'PRICE', 'O.SIZE', 'SIZE', 'VALUE', 'ID'],
 ])
 
 let orderCount = 0
@@ -28,6 +28,7 @@ for (const order of openOrders) {
         order.limitPx,
         order.origSz,
         order.sz,
+        new NiceNumber(Number(order.limitPx) * Number(order.origSz)).toSimpleCurrency(),
         order.oid,
     ])
 
